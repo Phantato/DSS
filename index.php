b0071f0d78a3e93b0397360328233aed6383ac95
ZHTan <tzh_1997@qq.com>
2016-12-07 01:03:02 +0800

    fitst commit

---
+++ index.php
@@ -0,0 +1,42 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>DSS</title>
+        <script type="text/javascript">
+            function userop(url)
+            {
+                    form=document.getElementsByClassName('userop')[0];
+                    form.action=url;
+                    form.submit();
+            }
+        </script>
+    </head>
+    <body>
+<?php
+if(isset($_SESSION["id"]))
+{
+    echo "<p>Welcom, " . $_SESSION["user"] . "</p>";
+?>
+<button type="button" name="logout" onclick="javascript:window.location.href='./user/logout.php'">logout</button>
+<?php
+}
+else
+{
+?>
+        <form class="userop" action="" method="post">
+            <input type="text" name="name" value="" placeholder="username" />
+            <input type="password" name="pass" value="" placeholder="password" />
+            <input type="button" name="login" value="login" onclick="javascript:userop('user/login.php')">
+            <input type="button" name="register" value="register" onclick="javascript:userop('user/register.php')">
+        </form>
+
+<?php
+}
+?>
+    </body>
+</html>
---
+++ user/login.php
@@ -0,0 +1,14 @@
+<?php
+$dss = new mysqli("localhost", "root", "rootadmin", "DSS");
+$sql = "SELECT * FROM `DSS_user` WHERE username='" . $_POST["name"] . "'";
+$res = $dss->query($sql)->fetch_assoc();
+if($res["password"] == $_POST["pass"])
+{
+    session_start();
+    $_SESSION["id"] = $res["uid"];
+    $_SESSION["user"] = $res["nickname"];
+}
+?>
+<script type="text/javascript">
+    history.go(-1)
+</script>
